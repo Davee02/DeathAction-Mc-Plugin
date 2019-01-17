3bcc9a83e2b232a31439040a33b42abc824b6bd1
David <davidhodel6@gmail.com>
2019-01-17 18:04:12 +0100

    Added plugin.yml

---
+++ src/ch/david/deathAction/DeathAction.java
@@ -0,0 +1,16 @@
+package ch.david.deathAction;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathAction extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server disables the plugin
+    }
+}
